3d27af20b0267a8228d4c15e2ba2c69d8146573d
onliwad101 <55780641+onliwad101@users.noreply.github.com>
2024-07-17 11:09:18 +0900

    update

--- eval/per-channel-weight-per-token-activation-quant/lm-evaluation-harness/run.sh
+++ eval/per-channel-weight-per-token-activation-quant/lm-evaluation-harness/run.sh
@@ -1,10 +1,10 @@
 ### LRQ W4A8KV8
-CUDA_VISIBLE_DEVICES=0 python main.py     --model hf-causal-experimental     --model_args pretrained=Llama-2-7b-hf-LRQ-w4a8     --tasks openbookqa,arc_easy,winogrande,hellaswag,arc_challenge,piqa,boolq    --num_fewshot 0    --no_cache
-CUDA_VISIBLE_DEVICES=0 python main.py     --model hf-causal-experimental     --model_args pretrained=Llama-2-13b-hf-LRQ-w4a8     --tasks openbookqa,arc_easy,winogrande,hellaswag,arc_challenge,piqa,boolq    --num_fewshot 0    --no_cache
-CUDA_VISIBLE_DEVICES=0,1 python main.py     --model hf-causal-experimental     --model_args pretrained=Llama-2-70b-hf-LRQ-w4a8     --tasks openbookqa,arc_easy,winogrande,hellaswag,arc_challenge,piqa,boolq    --num_fewshot 0    --no_cache
+CUDA_VISIBLE_DEVICES=0 python main.py     --model hf-causal-experimental     --model_args pretrained=onliwad101/Llama-2-7b-hf-LRQ-w4a8     --tasks openbookqa,arc_easy,winogrande,hellaswag,arc_challenge,piqa,boolq    --num_fewshot 0    --no_cache
+CUDA_VISIBLE_DEVICES=0 python main.py     --model hf-causal-experimental     --model_args pretrained=onliwad101/Llama-2-13b-hf-LRQ-w4a8     --tasks openbookqa,arc_easy,winogrande,hellaswag,arc_challenge,piqa,boolq    --num_fewshot 0    --no_cache
+CUDA_VISIBLE_DEVICES=0,1 python main.py     --model hf-causal-experimental     --model_args pretrained=onliwad101/Llama-2-70b-hf-LRQ-w4a8     --tasks openbookqa,arc_easy,winogrande,hellaswag,arc_challenge,piqa,boolq    --num_fewshot 0    --no_cache
 
 ### FlexRound W4A8KV8
-CUDA_VISIBLE_DEVICES=0 python main.py     --model hf-causal-experimental     --model_args pretrained=Llama-2-7b-hf-FlexRound-w4a8     --tasks openbookqa,arc_easy,winogrande,hellaswag,arc_challenge,piqa,boolq    --num_fewshot 0    --no_cache
-CUDA_VISIBLE_DEVICES=0 python main.py     --model hf-causal-experimental     --model_args pretrained=Llama-2-13b-hf-FlexRound-w4a8     --tasks openbookqa,arc_easy,winogrande,hellaswag,arc_challenge,piqa,boolq    --num_fewshot 0    --no_cache
-CUDA_VISIBLE_DEVICES=0,1 python main.py     --model hf-causal-experimental     --model_args pretrained=Llama-2-70b-hf-FlexRound-w4a8     --tasks openbookqa,arc_easy,winogrande,hellaswag,arc_challenge,piqa,boolq    --num_fewshot 0    --no_cache
+CUDA_VISIBLE_DEVICES=0 python main.py     --model hf-causal-experimental     --model_args pretrained=onliwad101/Llama-2-7b-hf-FlexRound-w4a8     --tasks openbookqa,arc_easy,winogrande,hellaswag,arc_challenge,piqa,boolq    --num_fewshot 0    --no_cache
+CUDA_VISIBLE_DEVICES=0 python main.py     --model hf-causal-experimental     --model_args pretrained=onliwad101/Llama-2-13b-hf-FlexRound-w4a8     --tasks openbookqa,arc_easy,winogrande,hellaswag,arc_challenge,piqa,boolq    --num_fewshot 0    --no_cache
+CUDA_VISIBLE_DEVICES=0,1 python main.py     --model hf-causal-experimental     --model_args pretrained=onliwad101/Llama-2-70b-hf-FlexRound-w4a8     --tasks openbookqa,arc_easy,winogrande,hellaswag,arc_challenge,piqa,boolq    --num_fewshot 0    --no_cache
 
