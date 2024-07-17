f8454725f30cf834bdeff1c247d33e553154bbfd
onliwad101 <55780641+onliwad101@users.noreply.github.com>
2024-07-17 11:07:08 +0900

    update

--- eval/per-channel-weight-only-quant/wikitext2/run.sh
+++ eval/per-channel-weight-only-quant/wikitext2/run.sh
@@ -1,21 +1,21 @@
 ### LRQ W4A16KV16
-CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model Llama-2-7b-hf-LRQ-w4a16
-CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model Llama-2-13b-hf-LRQ-w4a16
-CUDA_VISIBLE_DEVICES=0,1 python evaluate_hf.py --model Llama-2-70b-hf-LRQ-w4a16
+CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model onliwad101/Llama-2-7b-hf-LRQ-w4a16
+CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model onliwad101/Llama-2-13b-hf-LRQ-w4a16
+CUDA_VISIBLE_DEVICES=0,1 python evaluate_hf.py --model onliwad101/Llama-2-70b-hf-LRQ-w4a16
 
 ### LRQ W3A16KV16
-CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model Llama-2-7b-hf-LRQ-w3a16
-CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model Llama-2-13b-hf-LRQ-w3a16
-CUDA_VISIBLE_DEVICES=0,1 python evaluate_hf.py --model Llama-2-70b-hf-LRQ-w3a16
+CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model onliwad101/Llama-2-7b-hf-LRQ-w3a16
+CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model onliwad101/Llama-2-13b-hf-LRQ-w3a16
+CUDA_VISIBLE_DEVICES=0,1 python evaluate_hf.py --model onliwad101/Llama-2-70b-hf-LRQ-w3a16
 
 ### FlexRound W4A16KV16
-CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model Llama-2-7b-hf-FlexRound-w4a16
-CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model Llama-2-13b-hf-FlexRound-w4a16
-CUDA_VISIBLE_DEVICES=0,1 python evaluate_hf.py --model Llama-2-70b-hf-FlexRound-w4a16
+CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model onliwad101/Llama-2-7b-hf-FlexRound-w4a16
+CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model onliwad101/Llama-2-13b-hf-FlexRound-w4a16
+CUDA_VISIBLE_DEVICES=0,1 python evaluate_hf.py --model onliwad101/Llama-2-70b-hf-FlexRound-w4a16
 
 ### FlexRound W3A16KV16
-CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model Llama-2-7b-hf-FlexRound-w3a16
-CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model Llama-2-13b-hf-FlexRound-w3a16
-CUDA_VISIBLE_DEVICES=0,1 python evaluate_hf.py --model Llama-2-70b-hf-FlexRound-w3a16
+CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model onliwad101/Llama-2-7b-hf-FlexRound-w3a16
+CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model onliwad101/Llama-2-13b-hf-FlexRound-w3a16
+CUDA_VISIBLE_DEVICES=0,1 python evaluate_hf.py --model onliwad101/Llama-2-70b-hf-FlexRound-w3a16
 
 
