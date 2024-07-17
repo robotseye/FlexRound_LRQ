afdf69698387577fa5419d4f48b2139c67a8f888
onliwad101 <55780641+onliwad101@users.noreply.github.com>
2024-07-17 11:09:41 +0900

    update

--- eval/per-channel-weight-per-token-activation-quant/mmlu/run.sh
+++ eval/per-channel-weight-per-token-activation-quant/mmlu/run.sh
@@ -1,10 +1,10 @@
 ### LRQ W4A8KV8
-CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model Llama-2-7b-hf-LRQ-w4a8 --per_token_act_quant --kv_output_quant
-CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model Llama-2-13b-hf-LRQ-w4a8 --per_token_act_quant --kv_output_quant
-CUDA_VISIBLE_DEVICES=0,1 python evaluate_hf.py --model Llama-2-70b-hf-LRQ-w4a8 --per_token_act_quant --kv_output_quant
+CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model onliwad101/Llama-2-7b-hf-LRQ-w4a8 --per_token_act_quant --kv_output_quant
+CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model onliwad101/Llama-2-13b-hf-LRQ-w4a8 --per_token_act_quant --kv_output_quant
+CUDA_VISIBLE_DEVICES=0,1 python evaluate_hf.py --model onliwad101/Llama-2-70b-hf-LRQ-w4a8 --per_token_act_quant --kv_output_quant
 
 ### FlexRound W4A8KV8
-CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model Llama-2-7b-hf-FlexRound-w4a8 --per_token_act_quant --kv_output_quant
-CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model Llama-2-13b-hf-FlexRound-w4a8 --per_token_act_quant --kv_output_quant
-CUDA_VISIBLE_DEVICES=0,1 python evaluate_hf.py --model Llama-2-70b-hf-FlexRound-w4a8 --per_token_act_quant --kv_output_quant
+CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model onliwad101/Llama-2-7b-hf-FlexRound-w4a8 --per_token_act_quant --kv_output_quant
+CUDA_VISIBLE_DEVICES=0 python evaluate_hf.py --model onliwad101/Llama-2-13b-hf-FlexRound-w4a8 --per_token_act_quant --kv_output_quant
+CUDA_VISIBLE_DEVICES=0,1 python evaluate_hf.py --model onliwad101/Llama-2-70b-hf-FlexRound-w4a8 --per_token_act_quant --kv_output_quant
 
